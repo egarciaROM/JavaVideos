34d1c413011b6a390341f3908118ceecd0de7523
Eduardo Garcia Roman <edyg1995@gmail.com>
2022-10-28 01:04:38 -0400

    Java Videos Second Commit

---
+++ src/main/java/com/mywork/section2/VariableFun.java
@@ -0,0 +1,14 @@
+package com.mywork.section2;
+
+public class VariableFun {
+    public static void main(String... args){
+        int myAge; //declaration
+        myAge = 19; //initialization
+        String myHometown = "Cuernavaca";
+        final int SOME_NUM = 150;
+        String name = "Billy";
+        System.out.print(name + " is " + myAge + '\n');
+        System.out.print("My hometown is " + myHometown);
+        //End of video 2_3
+    }
+}
