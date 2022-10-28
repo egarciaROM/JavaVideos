f14ce2f1f5fccda2423f759053e8df5d11c0812d
Eduardo Garcia Roman <edyg1995@gmail.com>
2022-10-28 16:42:16 -0400

    Within the 2_5 video

---
+++ src/main/java/com/mywork/section2/CommentFun.java
@@ -0,0 +1,15 @@
+package com.mywork.section2;
+
+/*
+*   Eduardo Garcia
+*   Learning some Java
+*   This is a multiline comment
+* */
+
+//Video 2_5
+public class CommentFun {
+    public static void main(String... args){
+        System.out.println("Printing stuff");
+
+    }//end main
+}
