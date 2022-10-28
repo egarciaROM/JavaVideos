62a2ec1294b90f8a9b5a37b5ec428a0f815231f5
Eduardo Garcia Roman <edyg1995@gmail.com>
2022-10-28 16:27:32 -0400

    End of 2_4 video, ConversionFun

---
+++ src/main/java/com/mywork/section2/ConversionFun.java
@@ -0,0 +1,26 @@
+package com.mywork.section2;
+
+public class ConversionFun {
+    public static void main(String... args){
+        double myDouble = 3.14;
+        float myFloat = 3.14f; //Value has datatype so we add 'f'. Its a narrowing/lossy conversion
+        /*
+        Bigger bucket into smaller cup
+         */
+
+        double yourDouble = myFloat; //widening/lossless conversion(little cup into bigger bucket)
+        /*
+        * byte
+        * int
+        * double
+        * char
+        * long
+        * float
+        * boolean
+        * short
+        * */
+        //End of video 2_4
+
+
+    }
+}
